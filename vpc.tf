e1325c8ac8bd3150d02343cb1cc926a1a5870058
root <rupak>
2020-09-15 19:23:49 +0000

    Added VPC module new

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,19 @@
 module "vpc" {
   source  = "app.terraform.io/arb-testing/vpc/aws"
   version = "2.51.0"
-   cidr_block           = var.address_space
-  enable_dns_hostnames = true
+
+  name = "my-vpc"
+  cidr = "169.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["169.0.1.0/24", "169.0.2.0/24", "169.0.3.0/24"]
+  public_subnets  = ["169.0.101.0/24", "169.0.102.0/24", "169.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    name = "${var.prefix}-vpc"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
