8daf35d635e87606f052cca79a90aa4ec4412735
root <rupak>
2020-09-15 19:27:18 +0000

    updated azs  module new

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "169.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["169.0.1.0/24", "169.0.2.0/24", "169.0.3.0/24"]
   public_subnets  = ["169.0.101.0/24", "169.0.102.0/24", "169.0.103.0/24"]
 
